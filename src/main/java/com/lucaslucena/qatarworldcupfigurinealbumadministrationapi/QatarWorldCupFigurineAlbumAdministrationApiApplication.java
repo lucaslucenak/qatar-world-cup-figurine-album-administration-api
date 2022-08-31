c10393da7611fc1a53fce896f48484e3c02875a4
Lucas Lucena <lucas.lucenak@gmail.com>
2022-08-30 21:41:31 -0300

    Add Spring Project

---
+++ src/main/java/com/lucaslucena/qatarworldcupfigurinealbumadministrationapi/QatarWorldCupFigurineAlbumAdministrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucaslucena.qatarworldcupfigurinealbumadministrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QatarWorldCupFigurineAlbumAdministrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QatarWorldCupFigurineAlbumAdministrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucaslucena/qatarworldcupfigurinealbumadministrationapi/QatarWorldCupFigurineAlbumAdministrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaslucena.qatarworldcupfigurinealbumadministrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QatarWorldCupFigurineAlbumAdministrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
